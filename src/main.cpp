3e0587f5cf527919913f24f814f2c1a63a168b89
0xfarco <0xfarco@gmail.com>
2025-12-04 13:59:45 +0530

    add vertex array object (VAO)

--- src/main.cpp
+++ src/main.cpp
@@ -93,19 +93,27 @@ int main() {
         0.0f, 0.5f, 0.0f
     };
 
-    unsigned int VBO;
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
